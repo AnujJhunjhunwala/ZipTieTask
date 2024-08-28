cd07d35466ec0444130166c290c4cf7904e21d95
Anuj Jhunjhunwala <348612@uwr.edu.pl>
2024-08-28 20:40:40 +0200

    Add docstrings for functions

--- routers/authors.py
+++ routers/authors.py
@@ -10,6 +10,16 @@ from sqlalchemy import func, or_
 router = APIRouter(tags=["authors"])
 
 def get_db():
+    """
+    Dependency that provides a database session.
+
+    This function is used as a dependency in FastAPI to provide a database session
+    to the request handlers. It creates a new session, yields it for use, and ensures
+    that the session is closed after the request is processed.
+
+    Yields:
+        db (Session): A SQLAlchemy database session.
+    """
     db = SessionLocal()
     try:
         yield db
@@ -23,9 +33,11 @@ class AuthorRequest(BaseModel):
     last_name: str = Field(min_length=3, max_length=50)
     country: str = Field(min_length=3, max_length=50)
 
-# Add new author
 @router.post("/author", status_code=status.HTTP_201_CREATED)
 async def create_author(db: db_dependency, author_request: AuthorRequest):
+    """
+    This function creates a new author in the database.
+    """
     author_model = models.Author(
         first_name=author_request.first_name,
         last_name=author_request.last_name,
@@ -34,25 +46,31 @@ async def create_author(db: db_dependency, author_request: AuthorRequest):
     db.add(author_model)
     db.commit()
 
-# Fetch all authors
 @router.get("/authors", status_code=status.HTTP_200_OK)
 async def read_all_authors(db: db_dependency, limit: int = Query(default=2, gt=0), offset: int = Query(default=0, ge=0)):
+    """
+    This function fetches all authors from the database.
+    """
     authors = db.query(models.Author).offset(offset).limit(limit).all()
     return authors
 
-# Fetch author by ID
 @router.get("/author/{author_id}", status_code=status.HTTP_200_OK)
 async def read_author(db: db_dependency, author_id: int = Path(gt=0)):
+    """
+    This function fetches an author by ID from the database.
+    """
     author_model = db.query(models.Author).filter(models.Author.author_id == author_id).first()
     if author_model is not None:
         return author_model
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Author not found")
 
-# Update author by ID
 @router.put("/author/{author_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_author(db: db_dependency, 
                         author_id: int, 
                         author_request: AuthorRequest):
+    """
+    This function updates an author by ID in the database.
+    """
     author_model = db.query(models.Author).filter(models.Author.author_id == author_id).first()
     if author_model is None:
         raise HTTPException(status_code=404, detail="Author not found")
@@ -62,18 +80,23 @@ async def update_author(db: db_dependency,
     db.add(author_model)
     db.commit()
 
-# Delete author by ID
 @router.delete("/author/{author_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_author(db: db_dependency, author_id: int = Path(gt=0)):
+    """
+    This function deletes an author by ID from the database.
+    """
     author_model = db.query(models.Author).filter(models.Author.author_id == author_id).first()
     if author_model is None:
         raise HTTPException(status_code=404, detail="Author not found")
     db.query(models.Author).filter(models.Author.author_id == author_id).delete()
     db.commit()
 
-# Search authors by first name or last name
+# Search authors by name
 @router.get("/authors/search", status_code=status.HTTP_200_OK)
 async def search_authors(db: db_dependency, name: str = None, limit: int = Query(default=2, gt=0), offset: int = Query(default=0, ge=0)):
+    """
+    This function searches authors by name in the database. (User can enter any part of the name)
+    """
     if name is None:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Please enter a name to search")
     query = db.query(models.Author).filter(
--- routers/books.py
+++ routers/books.py
@@ -11,6 +11,16 @@ from sqlalchemy import func
 router = APIRouter(tags=["books"])
 
 def get_db():
+    """
+    Dependency that provides a database session.
+
+    This function is used as a dependency in FastAPI to provide a database session
+    to the request handlers. It creates a new session, yields it for use, and ensures
+    that the session is closed after the request is processed.
+
+    Yields:
+        db (Session): A SQLAlchemy database session.
+    """
     db = SessionLocal()
     try:
         yield db
@@ -26,9 +36,11 @@ class BookRequest(BaseModel):
     author_id: int = Field(gt=0)
     in_stock: bool = Field(default=True)
 
-# Add new book
 @router.post("/book", status_code=status.HTTP_201_CREATED)
 async def create_book(db: db_dependency, book_request: BookRequest):
+    """
+    This function creates a new book in the database.
+    """
     book_model = models.Book(**book_request.model_dump())
     try:
         db.add(book_model)
@@ -37,25 +49,31 @@ async def create_book(db: db_dependency, book_request: BookRequest):
         db.rollback()
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Author ID not exist.")
 
-# Fetch all books
 @router.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books(db: db_dependency, limit: int = Query(default=2, gt=0), offset: int = Query(default=0, ge=0)):
+    """
+    This function fetches all books from the database.
+    """
     books = db.query(models.Book).offset(offset).limit(limit).all()
     return books
 
-# Fetch book by ID
 @router.get("/book/{book_id}", status_code=status.HTTP_200_OK)
 async def read_book(db: db_dependency, book_id: int = Path(gt=0)):
+    """
+    This function fetches a book by ID from the database.
+    """
     book_model = db.query(models.Book).filter(models.Book.book_id == book_id).first()
     if book_model is not None:
         return book_model
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-# Update book 
 @router.put("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_book(db: db_dependency, 
                         book_id: int, 
                         book_request: BookRequest):
+    """
+    This function updates a book by ID in the database.
+    """
     book_model = db.query(models.Book).filter(models.Book.book_id == book_id).first()
     if book_model is None:
         raise HTTPException(status_code=404, detail="Book not found")
@@ -67,34 +85,42 @@ async def update_book(db: db_dependency,
     db.add(book_model)
     db.commit()
 
-# Delete book
 @router.delete("/book/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_book(db: db_dependency, book_id: int = Path(gt=0)):
+    """
+    This function deletes a book by ID from the database.
+    """
     book_model = db.query(models.Book).filter(models.Book.book_id == book_id).first()
     if book_model is None:
         raise HTTPException(status_code=404, detail="Book not found")
     db.query(models.Book).filter(models.Book.book_id == book_id).delete()
     db.commit()
 
-# Search books by published year
 @router.get("/books/year/{year_published}", status_code=status.HTTP_200_OK)
 async def search_books_by_year(db: db_dependency, year_published: int = Path(gt=1000), limit: int = Query(default=2, gt=0), offset: int = Query(default=0, ge=0)):
+    """
+    This function searches books by year in the database.
+    """
     books_search = db.query(models.Book).filter(models.Book.year_published == year_published).offset(offset).limit(limit).all()
     if len(books_search) != 0:
         return books_search
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No books in stock for year "+str(year_published))
 
-# Check availability of book
 @router.get("/book/{book_id}/availability", status_code=status.HTTP_200_OK)
 async def check_book_availability(db: db_dependency, book_id: int = Path(gt=0)):
+    """
+    This function checks the availability of a book in the database.
+    """
     book_model = db.query(models.Book).filter(models.Book.book_id == book_id).first()
     if book_model is not None:
         return {"in_stock": book_model.in_stock}
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
 
-# Get books by country of author
 @router.get("/books/author_country/{country}", status_code=status.HTTP_200_OK)
 async def get_books_by_author_country(db: db_dependency, country: str, limit: int = Query(default=2, gt=0), offset: int = Query(default=0, ge=0)):
+    """
+    This function fetches books by country of author from the database
+    """
     books_search = db.query(models.Book).join(models.Author).filter(func.lower(models.Author.country) == country.lower()).offset(offset).limit(limit).all()
     if len(books_search) != 0:
         return books_search
