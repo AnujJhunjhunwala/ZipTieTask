acc5445a16f34ec8895376c6719c616ae6c91640
Anuj Jhunjhunwala <348612@uwr.edu.pl>
2024-08-29 22:02:25 +0200

    Redirect to Swagger UI from root.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import FastAPI, responses
 import models
 from database import engine
-import models
-
 from routers import authors, books
 
 app = FastAPI()
@@ -10,3 +8,7 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(authors.router)
 app.include_router(books.router)
+
+@app.get("/")
+def redirect_to_docs():
+    return responses.RedirectResponse(url="/docs")
