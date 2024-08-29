451076c52a14bed7e85ffeffd612d08e5bde8e2c
Anuj Jhunjhunwala <348612@uwr.edu.pl>
2024-08-29 22:04:54 +0200

    Make connection to the database dynamic.

--- database.py
+++ database.py
@@ -2,8 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils import database_exists, create_database
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = 'mysql+pymysql://root:anuj1234@localhost:3306/ziptie'
+# Load environment variables from .env file
+load_dotenv()
+
+# Access the environment variables
+db_username = os.getenv('DB_USERNAME')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+
+# Construct the connection URL
+DATABASE_URL = f"mysql+pymysql://{db_username}:{db_password}@{db_host}:{db_port}/{db_name}"
+
+print(DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 
